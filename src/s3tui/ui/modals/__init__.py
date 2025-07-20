0d1659bcd8b5a56fc9a2cca47d4fcca335f09cca
Sharashchandra <sharashchandra1998@gmail.com>
2025-07-20 20:47:01 +0530

    feat(delete): Added delete modal, functionality to delete files and folders from s3

--- src/s3tui/ui/modals/__init__.py
+++ src/s3tui/ui/modals/__init__.py
@@ -1,5 +1,6 @@
 """Init file for modals package."""
 
+from .delete_modal import DeleteModal
 from .download_modal import DownloadModal
 
-__all__ = ["DownloadModal"]
+__all__ = ["DownloadModal", "DeleteModal"]
