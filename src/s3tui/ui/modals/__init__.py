625de2d9cb02e88b01f8c9088a8ca32fb4cad827
Sharashchandra <sharashchandra1998@gmail.com>
2025-07-23 13:07:10 +0530

    refactor(ui): Removed all existing UI code, adding title bar

--- src/s3tui/ui/modals/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Init file for modals package."""
-
-from .delete_modal import DeleteModal
-from .download_modal import DownloadModal
-from .upload_modal import UploadModal
-
-__all__ = ["DownloadModal", "DeleteModal", "UploadModal"]
--- src/s3tui/ui/widgets/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Widgets package for S3TUI."""
-
-from .bucket_list import BucketList
-from .object_list import ObjectList
-from .path_bar import PathBar
-
-__all__ = ["BucketList", "ObjectList", "PathBar"]
