3af6423eba30b49bfa2213d33a9900f896ff6909
Sharashchandra <sharashchandra1998@gmail.com>
2025-07-25 23:16:16 +0530

    refactor(delete): Added delete modal for files and folders

--- src/s3tui/ui/modals/__init__.py
+++ src/s3tui/ui/modals/__init__.py
@@ -1,5 +1,6 @@
 """Modal dialogs for S3TUI."""
 
+from .delete_modal import DeleteModal
 from .download_modal import DownloadModal
 
-__all__ = ["DownloadModal"]
+__all__ = ["DeleteModal", "DownloadModal"]
